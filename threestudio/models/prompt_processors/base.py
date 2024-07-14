9b178db96cacd12d640595553c3d8e86a1f53ab6
wooseok <wssong97@naver.com>
2024-07-14 12:28:30 +0900

    Fix: Added null prompts in the base class

--- threestudio/models/prompt_processors/base.py
+++ threestudio/models/prompt_processors/base.py
@@ -377,6 +377,7 @@ class PromptProcessor(BaseObject):
             + [self.negative_prompt]
             + self.prompts_vd
             + self.negative_prompts_vd
+            + [""]  # null text
         )
         prompts_to_process = []
         for prompt in all_prompts:
